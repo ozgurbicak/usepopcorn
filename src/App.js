ecab8f39cbb2a83d36247ae30763b440938fd7e9
Ozgur Bicak <ozgurbicak62@gmail.com>
2023-09-12 05:24:46 +0300

    i

--- src/App.js
+++ src/App.js
@@ -55,10 +55,15 @@ const KEY = "86a11b9c";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   // useEffect(function () {
   //   console.log("A");
@@ -80,6 +85,13 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
